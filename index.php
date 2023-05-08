9d494a9da8b0c07d2f868b312cac7951e949a91e
edoardorizzo <edo96.rizzo@gmail.com>
2023-05-08 16:42:15 +0200

    print on the same page the results of the json file

--- index.php
+++ index.php
@@ -1,34 +1,49 @@
-<?php
-$todos = [
-    'Dormire',
-    'Fare la spesa',
-];
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $new_todo = $_POST['todo'];
-  if ($new_todo) {
-    array_push($todos, $new_todo); // aggiungi il nuovo todo all'inizio dell'array
-  }
-}
-
-?>
-
 <!DOCTYPE html>
 <html>
+
 <head>
-  <title>Todo List</title>
+    <title>Todo List</title>
 </head>
+
 <body>
-  <h1>Todo List</h1>
-  <form method="post">
-    <label for="todo">Nuovo Todo:</label>
-    <input type="text" name="todo" id="todo">
-    <button type="submit">Aggiungi</button>
-  </form>
-  <ul>
-    <?php foreach ($todos as $todo) { ?>
-      <li><?= $todo ?></li>
-    <?php } ?>
-  </ul>
+    <h1>Todo List</h1>
+    <form method="post">
+        <label for="task">Task:</label>
+        <input type="text" name="task" id="task">
+        <button type="submit">Aggiungi</button>
+    </form>
+
+    <?php
+    // Verificare se il form è stato inviato
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        // Verificare se il campo task è stato compilato
+        if (!empty($_POST['task'])) {
+            // Aprire il file JSON per la scrittura
+            $file = 'tasks.json';
+            $data = file_exists($file) ? json_decode(file_get_contents($file), true) : [];
+
+            // Aggiungere il task al file JSON
+            $data[] = $_POST['task'];
+            file_put_contents($file, json_encode($data));
+        }
+    }
+
+    // Leggere i task dal file JSON
+    $file = 'tasks.json';
+    $data = file_exists($file) ? json_decode(file_get_contents($file), true) : [];
+
+    // Stampare i task
+    if (!empty($data)) {
+        echo '<h2>Task:</h2>';
+        echo '<ul>';
+        foreach ($data as $task) {
+            echo '<li>' . htmlspecialchars($task) . '</li>';
+        }
+        echo '</ul>';
+    } else {
+        echo '<p>Non ci sono task da mostrare.</p>';
+    }
+    ?>
 </body>
+
 </html>
