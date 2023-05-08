4863fc7dca73453d2175839e93d33ceb2014cebf
edoardorizzo <edo96.rizzo@gmail.com>
2023-05-08 16:20:24 +0200

    add task to the array

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+$todos = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $new_todo = $_POST['todo'];
+  if ($new_todo) {
+    array_unshift($todos, $new_todo); // aggiungi il nuovo todo all'inizio dell'array
+  }
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Todo List</title>
+</head>
+<body>
+  <h1>Todo List</h1>
+  <form method="post">
+    <label for="todo">Nuovo Todo:</label>
+    <input type="text" name="todo" id="todo">
+    <button type="submit">Aggiungi</button>
+  </form>
+  <ul>
+    <?php foreach ($todos as $todo) { ?>
+      <li><?= $todo ?></li>
+    <?php } ?>
+  </ul>
+</body>
+</html>
